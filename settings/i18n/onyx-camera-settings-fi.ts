8046fa1b1c42c2816389cc263e4f43a7407e7000
Kimmo Lindholm <kimmo.lindholm@eke.fi>
2016-04-10 00:20:53 +0300

    finnish translation

---
+++ settings/i18n/onyx-camera-settings-fi.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.1">
+<context>
+    <name/>
+    <message id="onyx-camera-settings-title">
+        <source>Camera settings</source>
+        <extracomment>page header</extracomment>
+        <translation>Kameran asetukset</translation>
+    </message>
+    <message id="primary-camera-settings">
+        <source>Rear camera</source>
+        <extracomment>section header for primary camera settings</extracomment>
+        <translation>Takakamera</translation>
+    </message>
+    <message id="image-resolution">
+        <source>Image resolution</source>
+        <extracomment>Combobox label for image resolution</extracomment>
+        <translation>Kuvan resoluutio</translation>
+    </message>
+    <message id="video-resolution">
+        <source>Video resolution</source>
+        <extracomment>Combobox label for video resolution</extracomment>
+        <translation>Videon resoluutio</translation>
+    </message>
+    <message id="secondary-camera-settings">
+        <source>Front camera</source>
+        <extracomment>section header for secondary camera settings</extracomment>
+        <translation>Etukamera</translation>
+    </message>
+</context>
+</TS>
