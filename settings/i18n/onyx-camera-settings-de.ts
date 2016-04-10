ce604d889632b36105b76a9cd842f6a49317c34c
Kimmo Lindholm <kimmo.lindholm@eke.fi>
2016-04-10 13:07:55 +0300

    add translations; de

---
+++ settings/i18n/onyx-camera-settings-de.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
+<context>
+    <name/>
+    <message id="onyx-camera-settings-title">
+        <source>Camera settings</source>
+        <extracomment>page header</extracomment>
+        <translation>Kamera Einstellungen</translation>
+    </message>
+    <message id="primary-camera-settings">
+        <source>Rear camera</source>
+        <extracomment>section header for primary camera settings</extracomment>
+        <translation>Hauptkamera</translation>
+    </message>
+    <message id="image-resolution">
+        <source>Image resolution</source>
+        <extracomment>Combobox label for image resolution</extracomment>
+        <translation>Bild Auflösung</translation>
+    </message>
+    <message id="video-resolution">
+        <source>Video resolution</source>
+        <extracomment>Combobox label for video resolution</extracomment>
+        <translation>Video Auflösung</translation>
+    </message>
+    <message id="secondary-camera-settings">
+        <source>Front camera</source>
+        <extracomment>section header for secondary camera settings</extracomment>
+        <translation>Frontkamera</translation>
+    </message>
+    <message id="other-settings-header">
+        <source>Other settings</source>
+        <extracomment>section header for other settings</extracomment>
+        <translation>Andere Einstellungen</translation>
+    </message>
+    <message id="video-bit-rate-slider-label">
+        <source>Video Bitrate</source>
+        <extracomment>Slider label for video bit rate setting</extracomment>
+        <translation>Video Bitrate</translation>
+    </message>
+</context>
+</TS>
