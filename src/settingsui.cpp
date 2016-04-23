4195b25e2adfa7c7e08b9cf8e7775997ea3d37a7
Kimmo Lindholm <kimmo.lindholm@eke.fi>
2016-04-23 15:25:53 +0300

    test7

--- src/settingsui.cpp
+++ src/settingsui.cpp
@@ -1,6 +1,7 @@
 #include "settingsui.h"
 #include <QDir>
 #include <QDebug>
+#include <QSize>
 #include <gst/gst.h>
 
 SettingsUi::SettingsUi(QObject *parent) :
@@ -18,25 +19,29 @@ SettingsUi::~SettingsUi()
 void SettingsUi::scan()
 {
     QList<QPair<QString, QVariant> > devices;
+    QList<QSize> res;
 
     // Too bad there's no way to get the values of an enum without creating the element :(
     GstElement *elem = gst_element_factory_make("droidcamsrc", NULL);
-    if (!elem) {
-      qWarning() << "QtCamScanner: Failed to create an instance of droidcamsrc";
-      return;
+    if (!elem)
+    {
+        qWarning() << "QtCamScanner: Failed to create an instance of droidcamsrc";
+        return;
     }
 
     GParamSpec *spec = g_object_class_find_property(G_OBJECT_GET_CLASS(elem), "camera-device");
-    if (!spec) {
-      qWarning() << "QtCamScanner: Failed to get property caemra-device";
-      gst_object_unref(elem);
-      return;
+    if (!spec)
+    {
+        qWarning() << "QtCamScanner: Failed to get property camera-device";
+        gst_object_unref(elem);
+        return;
     }
 
-    if (!G_IS_PARAM_SPEC_ENUM(spec)) {
-      qWarning() << "QtCamScanner: Property camera-device is not an enum";
-      gst_object_unref(elem);
-      return;
+    if (!G_IS_PARAM_SPEC_ENUM(spec))
+    {
+        qWarning() << "QtCamScanner: Property camera-device is not an enum";
+        gst_object_unref(elem);
+        return;
     }
 
     GParamSpecEnum *e = G_PARAM_SPEC_ENUM(spec);
@@ -46,14 +51,81 @@ void SettingsUi::scan()
 
     qDebug() << "default" << devices;
 
-    for (int x = e->enum_class->minimum; x <= e->enum_class->maximum; x++) {
-      if (x != e->default_value) {
-        devices << qMakePair<QString, QVariant>(e->enum_class->values[x].value_name,
-                            QByteArray::number(x));
-      }
+    for (int x = e->enum_class->minimum; x <= e->enum_class->maximum; x++)
+    {
+        if (x != e->default_value)
+        {
+            devices << qMakePair<QString, QVariant>(e->enum_class->values[x].value_name, QByteArray::number(x));
+        }
     }
 
     qDebug() << "all" << devices;
 
+    GstCaps *caps = NULL;
+    g_object_get(elem, "image-capture-supported-caps", &caps, NULL);
+
+    if (!caps)
+    {
+        qWarning() << "no caps";
+        gst_object_unref(elem);
+        return;
+    }
+
+    for (guint x = 0; x < gst_caps_get_size(caps); x++)
+    {
+        const GstStructure *s = gst_caps_get_structure(caps, x);
+        const GValue *width = gst_structure_get_value(s, "width");
+        const GValue *height = gst_structure_get_value(s, "height");
+
+        if (!width || !height)
+        {
+            qWarning() << "queryResolutions: no dimensions";
+            continue;
+        }
+
+        bool width_is_list = GST_VALUE_HOLDS_LIST(width) ? true : false;
+        bool height_is_list = GST_VALUE_HOLDS_LIST(height) ? true : false;
+
+        if (!width_is_list && !height_is_list)
+        {
+            res << QSize(g_value_get_int(width), g_value_get_int(height));
+        }
+        else if (width_is_list && height_is_list)
+        {
+            guint ws = gst_value_list_get_size(width);
+            guint hs = gst_value_list_get_size(width);
+            for (guint wc = 0; wc < ws; wc++)
+            {
+                int w = g_value_get_int(gst_value_list_get_value(width, wc));
+                for (guint hc = 0; hc < hs; hc++)
+                {
+                    int h = g_value_get_int(gst_value_list_get_value(height, hc));
+                    res << QSize(w, h);
+                }
+            }
+        }
+        else if (width_is_list)
+        {
+            int h = g_value_get_int(height);
+            for (guint i = 0; i < gst_value_list_get_size(width); i++)
+            {
+                int w = g_value_get_int(gst_value_list_get_value(width, i));
+                res << QSize(w, h);
+            }
+        }
+        else if (height_is_list)
+        {
+            int w = g_value_get_int(width);
+            for (guint i = 0; i < gst_value_list_get_size(height); i++)
+            {
+                int h = g_value_get_int(gst_value_list_get_value(height, i));
+                res << QSize(w, h);
+            }
+        }
+    }
+
+    qDebug() << res;
+
+    gst_caps_unref(caps);
     gst_object_unref(elem);
 }
