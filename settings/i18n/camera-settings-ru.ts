c4c0fb52501d3c519b5dcdac54d620b7c5ab96f8
Nikita Zhukov <yacuken@gmail.com>
2016-09-08 20:12:00 +0300

    add Russian translation

---
+++ settings/i18n/camera-settings-ru.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru_RU" version="2.1">
+<context>
+    <name/>
+    <message id="camera-settings-title">
+        <source>Camera settings</source>
+        <extracomment>page header</extracomment>
+        <translation>Настройки камеры</translation>
+    </message>
+    <message id="primary-camera-settings">
+        <source>Rear camera</source>
+        <extracomment>section header for primary camera settings</extracomment>
+        <translation>Основная камера</translation>
+    </message>
+    <message id="image-resolution">
+        <source>Image resolution</source>
+        <extracomment>Combobox label for image resolution</extracomment>
+        <translation>Разрешение изображения</translation>
+    </message>
+    <message id="video-resolution">
+        <source>Video resolution</source>
+        <extracomment>Combobox label for video resolution</extracomment>
+        <translation>Разрешение видео</translation>
+    </message>
+    <message id="secondary-camera-settings">
+        <source>Front camera</source>
+        <extracomment>section header for secondary camera settings</extracomment>
+        <translation>Передняя камера</translation>
+    </message>
+    <message id="other-settings-header">
+        <source>Other settings</source>
+        <extracomment>section header for other settings</extracomment>
+        <translation>Другие настройки</translation>
+    </message>
+    <message id="video-bit-rate-slider-label">
+        <source>Video Bitrate</source>
+        <extracomment>Slider label for video bit rate setting</extracomment>
+        <translation>Битрейт видео</translation>
+    </message>
+</context>
+</TS>
